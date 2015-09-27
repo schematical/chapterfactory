885e8e77d8f86d540f60a49d9e48b1d753d166b5
Matt Lea <mlea@schematical.com>
2015-09-27 13:40:37 -0500

    Getting geared up

--- gruntfile.js
+++ gruntfile.js
@@ -1,16 +1,23 @@
 module.exports = function(grunt) {
 	require('jit-grunt')(grunt);
+	grunt.loadNpmTasks('grunt-contrib-clean');
 	grunt.loadNpmTasks('grunt-aws-s3');
 	grunt.loadNpmTasks('grunt-angular-templates');
 	grunt.loadNpmTasks('grunt-angular-builder');
 	grunt.loadNpmTasks('grunt-bower');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-concat');
 
 	var config = {
 		aws:grunt.file.readJSON('aws-keys.json')
 	};
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
+		clean: {
+			build:[
+				'public/_build/*'
+			]
+		},
 		less: {
 			development: {
 				options: {
@@ -59,7 +66,7 @@ module.exports = function(grunt) {
 		},
 		'angular-builder': {
 			options: {
-				mainModule: 'cfcore',
+				mainModule: '<%= pkg.name %>',
 				// angular-builder ignores these modules
 				externalModules:[
 					'ngRoute',
@@ -80,8 +87,9 @@ module.exports = function(grunt) {
 				},
 				assets: {
 					enabled: true,
-					targetDir: 'dist/'
+					targetDir: 'public/_build/'
 				},
+
 				src:  [
 					'public/js/**/*.js',
 					'node_modules/njax/public/js/**/*.js',
@@ -89,6 +97,9 @@ module.exports = function(grunt) {
 
 
 				],
+				/*forceInclude:[
+					'node_modules/njax/public/js/services/subscription.js',
+				],*/
 				dest: 'public/_build/js/<%= pkg.name %>.js'
 			},
 			prod: {
@@ -102,6 +113,7 @@ module.exports = function(grunt) {
 				src:  [
 					'public/js/**/*.js',
 					'node_modules/njax/public/js/**/*.js',
+					'node_modules/njax/public/js/services/*.js',
 					'public/_build/js/templates.js',
 
 				],
@@ -120,7 +132,7 @@ module.exports = function(grunt) {
 						}
 						return url;
 					},
-					module: 'cfcore',
+					module: '<%= pkg.name %>',
 					htmlmin:  { collapseWhitespace: true, collapseBooleanAttributes: true },
 					prefix: 'https://chapterfactory.re'// 'https://s3.amazonaws.com/townsquare-prod-assets'
 				}
@@ -137,9 +149,22 @@ module.exports = function(grunt) {
 						}
 						return url;
 					},
-					module: 'cfcore',
+					module: '<%= pkg.name %>',
 					prefix: 'http://local.100i.com:3030'
 				}
+			}
+		},
+		concat: {
+			options: {
+				separator: ';'
+			},
+			local: {
+				src: [
+					'public/_build/js/<%= pkg.name %>.js',
+					'node_modules/njax/public/js/builder*.js',
+					'node_modules/njax/public/js/services/*.js',
+				],
+				dest: 'public/_build/js/<%= pkg.name %>.concat.js'
 			}
 		},
 		bower: {
@@ -166,7 +191,7 @@ module.exports = function(grunt) {
 					soureMapInline: true
 				},
 				files: {
-					'public/_build/js/<%= pkg.name %>.min.js': ['public/_build/js/<%= pkg.name %>.js'],
+					'public/_build/js/<%= pkg.name %>.min.js': ['public/_build/js/<%= pkg.name %>.concat.js'],
 					'public/_build/js/vendor.js': [
 						'public/_build/bower/dist/jquery.js',
 						'public/_build/bower/dist/js/bootstrap.js',
@@ -206,12 +231,14 @@ module.exports = function(grunt) {
 		}
 	});
 
-
+	grunt.registerTask ('debug', ['angular-builder::debug']);
 
 	grunt.registerTask('build-local', [
+		'clean:build',
 		'less',
 		'ngtemplates:local',
 		'angular-builder:local',
+		'concat:local',
 		'bower:local',
 		'uglify:local'
 	]);
